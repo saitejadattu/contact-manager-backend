9d30fe67117dc4fb7b811d32395546ae5f2584d2
saitejadattu <164032855+saitejadattu@users.noreply.github.com>
2025-04-09 11:53:16 +0530

    Update dbConnection.js

--- dbConnection.js
+++ dbConnection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectToDB = () =>{
-    mongoose.connect("mongodb+srv://killer:GyisTzGlWFv8w0co@cluster0.j1ccr.mongodb.net/contact-manager")
+    mongoose.connect("mongodb+srv://killer:Dg1Z8DfSbQxm0b0t@cluster0.j1ccr.mongodb.net/contact-manager")
     .then(()=>console.log("mongoDB is connected"))
     .catch((err)=>console.log("database error", err.message))
 }
